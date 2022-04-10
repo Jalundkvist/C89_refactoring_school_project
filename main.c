27502e62525f4ed037585877b30051b0820d5360
Jacob Lundkvist <jacob@lundkvist.com>
2022-04-10 13:10:34 +0200

    main loop updated

--- main.c
+++ main.c
@@ -10,20 +10,12 @@
 ******************************************************************************/
 int main(void)
 {	
-	
 	setup();
 	
-	
-	long num1 = -25;
-	unsigned long num2 = 25;
-	serial_print("Test if working\n");
-	serial_print_integer("Test integer: %d \n", &num1);
-	serial_print_unsigned("Test unsigned: %u \n", &num2);
     while (true);
 	{
 		
 	}
-	
 	return 0;
 }
 
