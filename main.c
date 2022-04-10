d041192481114b7938b09507596e669fe48544dc
Jacob Lundkvist <jacob@lundkvist.com>
2022-04-10 14:37:15 +0200

    comment updated in main

--- main.c
+++ main.c
@@ -3,15 +3,13 @@
 
 /******************************************************************************
 * Funktionen main utgör programmets start- och slutpunkt. Programmets globala
-* variabler initieras via anrop av funktionen setup. En goto-sats används för
+* variabler initieras via anrop av funktionen setup. En while-loop används för
 * att hålla igång programmet så länge matningsspänning tillförs. Resterande
-* del av programmet är avbrottsgenererat, så hopp sker kontinuerligt till
-* adressen döpt loop.
+* del av programmet är avbrottsgenererat.
 ******************************************************************************/
 int main(void)
 {	
 	setup();
-	
     while (true);
 	{
 		
